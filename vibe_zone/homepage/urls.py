23d5f21ab7fde597e065ed14ca3309462562c6ec
androska <polesuke10@gmail.com>
2025-08-06 19:58:01 +0300

    css base+

--- vibe_zone/homepage/urls.py
+++ vibe_zone/homepage/urls.py
@@ -4,5 +4,9 @@ from django.views.generic import TemplateView
 app_name = "homepage"
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="homepage/base.html")),
+    path(
+        "", 
+        TemplateView.as_view(template_name="homepage/base.html"), 
+        name="home"
+        ),
 ]
